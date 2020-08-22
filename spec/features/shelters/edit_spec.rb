92dc1a0695bef214d24d91e7149d8f7126bed68e
RyanDBarnett <conner2468@gmail.com>
2020-08-22 09:55:50 -0600

    Add shelter update form test

---
+++ spec/features/shelters/edit_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+RSpec.describe 'Edit Shelter' do
+  describe 'As a visitor' do
+    describe 'When I visit the edit shelter form page' do
+      it 'I can update a shelter' do
+        @shelter_1 = Shelter.create(name: 'FurBabies4Ever', address: '1664 Poplar St', city: 'Denver', state: 'CO', zip: '80220')
+
+        visit "/shelters/#{@shelter_1.id}/edit"
+
+        fill_in 'Name', with: 'FurBabies4Life'
+        fill_in 'Address', with: '1500 Henry St'
+        fill_in 'City', with: 'Aurora'
+        fill_in 'State', with: 'Colorado'
+        fill_in 'Zip', with: '83062'
+        
+        click_on 'Update Shelter'
+
+        expect(current_path).to eq("/shelters")
+        expect(page).to have_content('FurBabies4Life')
+
+        click_on 'FurBabies4Life'
+
+        expect(page).to have_content('1500 Henry St')
+        expect(page).to have_content('Aurora')
+        expect(page).to have_content('Colorado')
+        expect(page).to have_content('83062')
+
+        expect(page).to_not have_content('1664 Poplar St')
+        expect(page).to_not have_content('Denver')
+        expect(page).to_not have_content('CO')
+        expect(page).to_not have_content('80220')
+      end
+    end
+  end
+end
