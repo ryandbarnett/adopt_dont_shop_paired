435360aad9c66354d260c60e456d11b9e896f3c9
RyanDBarnett <conner2468@gmail.com>
2020-08-22 15:05:33 -0600

    Add pets table to schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_19_223829) do
+ActiveRecord::Schema.define(version: 2020_08_22_204035) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "pets", force: :cascade do |t|
+    t.string "name"
+    t.string "image"
+    t.string "description"
+    t.string "age"
+    t.string "sex"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "shelters", force: :cascade do |t|
     t.string "name"
