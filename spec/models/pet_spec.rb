8813be6eb81b506c5bfbdbe53d327942e76c7536
RyanDBarnett <conner2468@gmail.com>
2020-08-22 15:21:32 -0600

    Change pet relationship model test to use belong_to instead of have_one

--- spec/models/pet_spec.rb
+++ spec/models/pet_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 describe Pet do
   describe 'relationships' do
-    it { should have_one :shelter }
+    it { should belong_to :shelter }
   end
-  
+
   describe "validations" do
     it { should validate_presence_of :name}
     it { should validate_presence_of :sex}
