992180d1ec9d68c62133ed9ef0c4c1d74cfc046d
RyanDBarnett <conner2468@gmail.com>
2020-08-22 23:56:29 -0600

    Add shelter create pet route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'welcome#index'
-  
+
   get '/shelters', to: 'shelters#index'
   get '/shelters/new', to: 'shelters#new'
   get '/shelters/:id', to: 'shelters#show'
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   get '/shelters/:id/edit', to: 'shelters#edit'
   patch 'shelters/:id', to: 'shelters#update'
   get '/shelters/:id/pets', to: 'pets#index'
+  get '/shelters/:id/pets/new', to: 'pets#new'
+  post '/shelters/:id/pets', to: 'pets#create'
 
   get '/pets', to: 'pets#index'
 
