ebf51fa6ccaacfd773ef340e2e93631d921edba4
RyanDBarnett <conner2468@gmail.com>
2020-08-23 13:34:48 -0600

    Add pets controller update and edit actions

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -9,12 +9,12 @@ class PetsController < ApplicationController
   end
 
   def new
-    @shelter_id = params[:id]
+
   end
 
   def create
     Pet.create(pet_params)
-    redirect_to "/shelters/#{pet_params[:shelter_id]}/pets"
+    redirect_to "/shelters/#{params[:id]}/pets"
   end
 
   def show
@@ -26,10 +26,23 @@ class PetsController < ApplicationController
     redirect_to '/pets'
   end
 
+  def edit
+    
+  end
+
+  def update
+    Pet.update(params[:id], update_pet_params)
+    redirect_to "/pets/#{params[:id]}"
+  end
+
   private
   def pet_params
     pet_params = params.permit(:name, :age, :sex, :image, :description, :id)
     pet_params[:shelter_id] = pet_params.delete(:id)
     pet_params
+  end
+
+  def update_pet_params
+    params.permit(:name, :age, :sex, :image, :description, :id, :shelter_id)
   end
 end
