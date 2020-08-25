a13829b86e1c9e800cad08fd4da3126757fbfb35
Philip DeFraties <philipjfry338@gmail.coom>
2020-08-24 19:59:01 -0600

    Add reviews model and table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_23_194524) do
+ActiveRecord::Schema.define(version: 2020_08_25_014848) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,15 @@ ActiveRecord::Schema.define(version: 2020_08_23_194524) do
     t.index ["shelter_id"], name: "index_pets_on_shelter_id"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.string "title"
+    t.integer "rating"
+    t.string "content"
+    t.string "image"
+    t.bigint "shelter_id"
+    t.index ["shelter_id"], name: "index_reviews_on_shelter_id"
+  end
+
   create_table "shelters", force: :cascade do |t|
     t.string "name"
     t.string "address"
@@ -39,4 +48,5 @@ ActiveRecord::Schema.define(version: 2020_08_23_194524) do
   end
 
   add_foreign_key "pets", "shelters"
+  add_foreign_key "reviews", "shelters"
 end
---
+++ spec/models/review_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe Review do
+  describe 'relationships' do
+    it { should belong_to :shelter }
+  end
+
+  describe "validations" do
+    it { should validate_presence_of :title}
+    it { should validate_presence_of :rating}
+    it { should validate_presence_of :content}
+    it { should validate_presence_of :image}
+  end
+end
