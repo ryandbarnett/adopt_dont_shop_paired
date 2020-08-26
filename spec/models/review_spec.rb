5fcb20bea7a889ea9708b4f74a6e904b89cfff2b
RyanDBarnett <conner2468@gmail.com>
2020-08-25 18:23:57 -0600

    Remove review model validate presence of image test

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -9,6 +9,5 @@ describe Review do
     it { should validate_presence_of :title}
     it { should validate_presence_of :rating}
     it { should validate_presence_of :content}
-    it { should validate_presence_of :image}
   end
 end
