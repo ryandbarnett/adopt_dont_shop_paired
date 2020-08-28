7e31b2d463196c57338d9e5e8f241acbd137dba0
Philip DeFraties <philipjfry338@gmail.coom>
2020-08-27 20:17:09 -0600

    Add index action.

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -6,4 +6,13 @@ class FavoritesController < ApplicationController
     redirect_to "/pets/#{pet.id}"
     flash[:notice] = "#{pet.name} added to favorites"
   end
+
+  def index
+    pet_id_strings = session[:favorites].map do |key, value|
+                        key.to_i
+                     end
+    @pets = pet_id_strings.map do |id|
+              Pet.find(id)
+            end
+  end
 end
