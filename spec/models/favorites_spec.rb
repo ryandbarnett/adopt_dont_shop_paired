3d60c2cfa7a4dd115ce49cf91e6ca2c7ce7b206e
Philip DeFraties <philipjfry338@gmail.coom>
2020-08-27 19:11:53 -0600

    Add test for add_pet method.

--- spec/models/favorites_spec.rb
+++ spec/models/favorites_spec.rb
@@ -10,5 +10,18 @@ describe Favorites do
 
       expect(favorites.total_count).to eq(2)
     end
+
+    describe "#add_pet" do
+      it "adds a pet to its contents" do
+        favorites = Favorites.new({
+          '1' => 1,
+          '2' => 1
+        })
+        favorites.add_pet(1)
+        favorites.add_pet(2)
+
+        expect(favorites.total_count).to eq(4)
+      end
+    end
   end
 end
