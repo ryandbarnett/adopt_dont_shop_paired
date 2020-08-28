f808e74a4b32369b03d22eac62165443c8e2edd1
Philip DeFraties <philipjfry338@gmail.coom>
2020-08-27 19:12:42 -0600

    Add add_pet and count_of methods.

--- app/poro/favorites.rb
+++ app/poro/favorites.rb
@@ -7,4 +7,12 @@ class Favorites
   def total_count
     @contents.values.sum
   end
+
+  def add_pet(id)
+    @contents[id.to_s] = count_of(id) + 1
+  end
+
+  def count_of(id)
+    @contents[id.to_s].to_i
+  end
 end
