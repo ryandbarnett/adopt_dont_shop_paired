7cbd9f4fe591032424f32184f6a470a29ecb4c70
Philip DeFraties <philipjfry338@gmail.coom>
2020-08-30 14:55:19 -0600

    Edit model inheritance.

--- app/models/application.rb
+++ app/models/application.rb
@@ -2,4 +2,6 @@ class Application < ApplicationRecord
   has_many :pet_applications
   has_many :pets, through: :pet_applications
   serialize :pet_ids, Array
+
+  validates_presence_of :name, :address, :city, :state, :zip, :phone_number, :description
 end
