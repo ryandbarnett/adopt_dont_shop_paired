f9307f25ff5cbf2c571408a87fdb1adbc8478b1e
Philip DeFraties <philipjfry338@gmail.coom>
2020-08-29 19:42:57 -0600

    Add application and pet_application tables.

---
+++ db/migrate/20200829225507_create_applications.rb
@@ -0,0 +1,13 @@
+class CreateApplications < ActiveRecord::Migration[5.2]
+  def change
+    create_table :applications do |t|
+      t.string :name
+      t.string :address
+      t.string :city
+      t.string :state
+      t.string :zip
+      t.string :phone_number
+      t.string :description
+    end
+  end
+end
