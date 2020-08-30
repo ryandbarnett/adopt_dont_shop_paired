e362da9edd20c1117f9d41af9e26fbb43b121115
Philip DeFraties <philipjfry338@gmail.coom>
2020-08-30 12:22:24 -0600

    Add create action and params

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -4,4 +4,13 @@ class ApplicationsController < ActionController::Base
     @favorite_pets = Pet.find(session[:favorites])
   end
 
+  def create
+    session[:favorites] -= params[:adopt][:pet_ids]
+    redirect_to '/favorites'
+    flash[:notice] = "Your application for your favorited pets has been submitted"
+  end
+
+  def application_params
+    params.require(:application).permit(:name, :address, :city, :state, :zip, :phone_number, :description, pet_ids:[])
+  end
 end
