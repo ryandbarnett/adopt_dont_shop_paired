14a545a22c340baa711a11a07b2d1a03a2c9796d
Philip DeFraties <philipjfry338@gmail.coom>
2020-08-31 21:05:57 -0600

    Add test for incomplete form sadpath

--- spec/features/shelters/edit_spec.rb
+++ spec/features/shelters/edit_spec.rb
@@ -29,5 +29,23 @@ RSpec.describe 'As a visitor' do
       expect(page).to_not have_content('CO')
       expect(page).to_not have_content('80220')
     end
+    it "I see a flash message when submitting an incomplete edit form" do
+
+      shelter_1 = Shelter.create(name: 'FurBabies4Ever', address: '1664 Poplar St', city: 'Denver', state: 'CO', zip: '80220')
+
+      visit "/shelters/#{shelter_1.id}/edit"
+
+      fill_in 'Name', with: 'FurBabies4Life'
+      fill_in 'Address', with: '1500 Henry St'
+      fill_in 'City', with: 'Aurora'
+      fill_in 'State', with: 'Colorado'
+      fill_in 'Zip', with: ''
+
+      click_on 'Update Shelter'
+
+      expect(current_path).to eq("/shelters/#{shelter_1.id}/edit")
+
+      expect(page).to have_content('Shelter update unsuccessful, form must have no empty fields')
+    end
   end
 end
