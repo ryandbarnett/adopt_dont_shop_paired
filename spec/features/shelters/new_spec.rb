45251c5baeb272a18c5ddb6e507a2739cfc1e59c
Philip DeFraties <philipjfry338@gmail.coom>
2020-08-31 20:55:50 -0600

    Add test for incomplete shelter creation form sadpath

--- spec/features/shelters/new_spec.rb
+++ spec/features/shelters/new_spec.rb
@@ -17,6 +17,21 @@ RSpec.describe "New Shelter" do
         expect(current_path).to eq("/shelters")
         expect(page).to have_content('Denver Animal Shelter')
       end
+      describe "If I leave any fields blank and click to create shelter" do
+        it "I see a flash message indicating failure of creation" do
+          visit '/shelters/new'
+
+          fill_in 'Name', with: 'Denver Animal Shelter'
+          fill_in 'Address', with: '1241 W. Bayaud Ave.'
+          fill_in 'City', with: 'Denver'
+          fill_in 'State', with: 'CO'
+
+          click_on 'Create Shelter'
+
+          expect(current_path).to eq('/shelters/new')
+          expect(page).to have_content('Shelter not created, form must have no empty fields')
+        end
+      end
     end
   end
 end
