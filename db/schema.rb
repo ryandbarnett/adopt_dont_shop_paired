33823e351503ef27496f4e90d1c4ba0f824b7b72
Philip DeFraties <philipjfry338@gmail.coom>
2020-09-02 16:52:42 -0600

    Add approved_app column to pets

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_31_011306) do
+ActiveRecord::Schema.define(version: 2020_09_02_222721) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 2020_08_31_011306) do
     t.datetime "updated_at", null: false
     t.bigint "shelter_id"
     t.string "status", default: "adoptable"
+    t.integer "approved_app"
     t.index ["shelter_id"], name: "index_pets_on_shelter_id"
   end
 
